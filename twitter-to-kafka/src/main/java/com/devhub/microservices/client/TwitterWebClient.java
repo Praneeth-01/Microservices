548afc585fcfbc24fd818f1af69ac8d139eecc3a
Praneeth-01 <nistalapraneeth@gmail.com>
2025-11-23 13:04:10 +0530

    Minor changes

--- twitter-to-kafka/src/main/java/com/devhub/microservices/client/TwitterWebClient.java
+++ twitter-to-kafka/src/main/java/com/devhub/microservices/client/TwitterWebClient.java
@@ -1,16 +1,24 @@
 package com.devhub.microservices.client;
 
 import com.devhub.microservices.twitter_mock.model.Status;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
 @Component
 public class TwitterWebClient {
+
+    @Value("${twitter.mock.url}")
+    private String twitterUrl;
+
     private final WebClient webClient;
 
     public TwitterWebClient() {
-        this.webClient = WebClient.builder().baseUrl("http://localhost:8080/twitter-stream").build();
+        if (twitterUrl == null) {
+            throw new IllegalArgumentException("Twitter URL is not set");
+        }
+        this.webClient = WebClient.builder().baseUrl(twitterUrl).build();
     }
 
     public Flux<Status> getTweets() {
